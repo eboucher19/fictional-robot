9ab52dd08eaf4e85767b9bd4c31fec5d023d01db
eboucher19 <33849312+eboucher19@users.noreply.github.com>
2017-11-20 20:12:30 -0400

    Add files to create my website

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+  console.log("It's working")
+  console.log("test")
+
+
+$("#close, #hamburger").click(function(){
+
+  $("nav ul").toggleClass("open-nav");
+
+
+
+})
+
+
+})
+
+
+//class selector
+// $(".cupcake").click(function)(){
+//   alert("this was clicked!");
+// })
